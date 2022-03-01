0c532d477feddcc43d3d93bd6bb5cc646db07177
Benjamin Barnickel <dev@bx11.de>
2022-03-01 22:43:46 +0100

    more checks

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,30 @@
 use std::fmt::Display;
 
-use rand::{Rng, prelude::SliceRandom};
+use rand::{prelude::SliceRandom, Rng};
+
+#[derive(Debug, Clone)]
+pub enum SteuerIdCheckError {
+    ZeroAtStart,
+    InvalidDigit(usize),
+    InvalidNumbers,
+    InvalidChecksum,
+}
+
+impl Display for SteuerIdCheckError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SteuerIdCheckError::ZeroAtStart => "0 als erste Ziffer".into(),
+                SteuerIdCheckError::InvalidDigit(pos) =>
+                    format!("Ziffer {0} ist nicht in 0..9", pos + 1),
+                SteuerIdCheckError::InvalidNumbers => "Ungültige Nummernfolge".into(),
+                SteuerIdCheckError::InvalidChecksum => "Ungültige Prüfziffer".into(),
+            }
+        )
+    }
+}
 
 pub struct SteuerId(pub [u8; 11]);
 
@@ -19,13 +43,9 @@ impl Display for SteuerId {
 }
 
 impl SteuerId {
-    pub fn try_create(digits: [u8; 11]) -> Result<Self, String> {
+    pub fn try_create(digits: [u8; 11]) -> Result<Self, SteuerIdCheckError> {
         let result = Self(digits);
-        if result.check() {
-            Ok(result)
-        } else {
-            Err("Prüfziffer stimmt nicht!".into())
-        }
+        result.check().map(|_| result)
     }
 
     pub fn create_random() -> Self {
@@ -50,10 +70,43 @@ impl SteuerId {
         Self(digits)
     }
 
-    fn check(&self) -> bool {
-        let calc_pz = calc_pz(&self.0[..11]);
-        let pz = self.0[10];
-        calc_pz == pz
+    fn check(&self) -> Result<(), SteuerIdCheckError> {
+        self.0
+            .iter()
+            .take(10)
+            .enumerate()
+            .fold(Ok([0usize; 10]), |a, (i, &d)| match a {
+                Err(e) => Err(e),
+                Ok(mut a) => match d {
+                    0 if i == 0 => Err(SteuerIdCheckError::ZeroAtStart),
+                    10.. => Err(SteuerIdCheckError::InvalidDigit(i)),
+                    _ => match a[d as usize] {
+                        3.. => Err(SteuerIdCheckError::InvalidNumbers),
+                        _ => {
+                            a[d as usize] += 1;
+                            Ok(a)
+                        }
+                    },
+                },
+            })
+            .and_then(|ca| {
+                match ca.iter().fold([0usize; 4], |mut ca, &c| {
+                    ca[c] += 1;
+                    ca
+                }) {
+                    [1, 8, 1, 0] => Ok(()),
+                    [2, 7, 0, 1] => Ok(()),
+                    _ => Err(SteuerIdCheckError::InvalidNumbers),
+                }
+            })
+            .and_then(|_| {
+                let pz = self.0[10];
+                if pz == calc_pz(&self.0[..11]) {
+                    Ok(())
+                } else {
+                    Err(SteuerIdCheckError::InvalidChecksum)
+                }
+            })
     }
 }
 
@@ -67,7 +120,7 @@ fn calc_pz(digits: &[u8]) -> u8 {
     });
     match 11 - product {
         10 => 0,
-        n => n
+        n => n,
     }
 }
 
@@ -76,7 +129,7 @@ mod test_model {
     use super::*;
 
     #[test]
-    fn test_create_valid() {
+    fn test_create_valid_one_replaced() {
         let id = SteuerId::try_create([1, 0, 3, 7, 4, 9, 1, 8, 2, 5, 8]);
         assert!(id.is_ok());
         let id = id.unwrap();
@@ -84,8 +137,46 @@ mod test_model {
     }
 
     #[test]
-    fn test_invalid() {
+    fn test_create_valid_two_replaced() {
+        let id = SteuerId::try_create([1, 0, 3, 7, 1, 9, 1, 8, 2, 5, 7]);
+        assert!(id.is_ok());
+        let id = id.unwrap();
+        assert_eq!(id.to_string(), "10371918257".to_owned());
+    }
+
+    #[test]
+    fn test_invalid_zero_at_start() {
+        let id = SteuerId::try_create([0, 1, 3, 7, 4, 9, 1, 8, 2, 5, 7]);
+        assert!(matches!(id, Err(SteuerIdCheckError::ZeroAtStart)));
+    }
+
+    #[test]
+    fn test_invalid_checksum() {
         let id = SteuerId::try_create([1, 0, 3, 7, 4, 9, 1, 8, 2, 5, 7]);
-        assert!(id.is_err());
+        assert!(matches!(id, Err(SteuerIdCheckError::InvalidChecksum)));
+    }
+
+    #[test]
+    fn test_invalid_digits() {
+        let id = SteuerId::try_create([1, 13, 3, 7, 4, 8, 1, 5, 2, 6, 8]);
+        assert!(matches!(id, Err(SteuerIdCheckError::InvalidDigit(1))));
+    }
+
+    #[test]
+    fn test_invalid_numbers_all_different() {
+        let id = SteuerId::try_create([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+        assert!(matches!(id, Err(SteuerIdCheckError::InvalidNumbers)));
+    }
+
+    #[test]
+    fn test_invalid_numbers_two_and_two() {
+        let id = SteuerId::try_create([1, 2, 1, 2, 5, 6, 7, 8, 9, 0, 1]);
+        assert!(matches!(id, Err(SteuerIdCheckError::InvalidNumbers)));
+    }
+
+    #[test]
+    fn test_invalid_numbers_more_than_three() {
+        let id = SteuerId::try_create([1, 1, 1, 2, 5, 6, 7, 1, 9, 0, 1]);
+        assert!(matches!(id, Err(SteuerIdCheckError::InvalidNumbers)));
     }
 }
